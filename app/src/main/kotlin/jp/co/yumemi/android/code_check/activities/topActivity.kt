15d7ad7d887db404e381d6a9c45ab36971bbbdec
pasangeek <pasan.madusanka9@gmail.com>
2024-04-26 21:29:28 +0900

    add end point to common network class

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/activities/topActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/activities/topActivity.kt
@@ -4,12 +4,11 @@
 package jp.co.yumemi.android.code_check.activities
 
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.code_check.R
 import java.util.*
-
+@AndroidEntryPoint
 class TopActivity : AppCompatActivity(R.layout.activity_top) {
 
-    companion object {
-        lateinit var lastSearchDate: Date
-    }
+
 }
