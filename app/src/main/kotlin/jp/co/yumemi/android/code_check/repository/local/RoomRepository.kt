1caa1cdaf0d347da986fc79a357de720d69a0f42
pasangeek <pasan.madusanka9@gmail.com>
2024-04-29 19:29:08 +0900

    implement Room repository class

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/local/RoomRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/local/RoomRepository.kt
@@ -1,4 +1,21 @@
 package jp.co.yumemi.android.code_check.repository.local
 
-class RoomRepository {
+import jp.co.yumemi.android.code_check.data.database.FavoriteRepositoryDao
+import jp.co.yumemi.android.code_check.data.database.entities.FavoriteRepositoryEntity
+import javax.inject.Inject
+
+class RoomRepository @Inject constructor(
+private val dao: FavoriteRepositoryDao){
+
+    suspend fun insert(repository: FavoriteRepositoryEntity) {
+        dao.insert(repository)
+    }
+
+    suspend fun getAllFavoriteRepositories(): List<FavoriteRepositoryEntity> {
+        return dao.getAll()
+    }
+
+    suspend fun deleteFavoriteRepository(repository: FavoriteRepositoryEntity) {
+        dao.delete(repository)
+    }
 }
