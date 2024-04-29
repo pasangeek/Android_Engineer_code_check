1a05c269d77e2d3ddc77a8c1a2abe2727ab639af
pasangeek <pasan.madusanka9@gmail.com>
2024-04-29 18:50:08 +0900

    impliment database module class

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/di/DatabaseModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/di/DatabaseModule.kt
@@ -1,4 +1,32 @@
 package jp.co.yumemi.android.code_check.di
 
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            "app_database"
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFavoriteRepositoryDao(appDatabase: AppDatabase): FavoriteRepositoryDao {
+        return appDatabase.favoriteRepositoryDao()
+    }
+
 }
