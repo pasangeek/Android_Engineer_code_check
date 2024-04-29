21a5030be471c474ddd46e5a2cf3839e47adac82
pasangeek <pasan.madusanka9@gmail.com>
2024-04-29 10:00:33 +0900

    bottom navigation implementation on top activity

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/activities/topActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/activities/topActivity.kt
@@ -3,14 +3,42 @@
  */
 package jp.co.yumemi.android.code_check.activities
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.code_check.R
+import jp.co.yumemi.android.code_check.databinding.ActivityTopBinding
 
 /**
  * Top-level activity in the application.
  * Annotated with [@AndroidEntryPoint] for Hilt dependency injection.
  */
 @AndroidEntryPoint
-class TopActivity : AppCompatActivity(R.layout.activity_top) {
-}
+class TopActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTopBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityTopBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.favouriteRepositoryFragment, R.id.settingFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+    }
